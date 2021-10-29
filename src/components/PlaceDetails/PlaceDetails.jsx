961b56b1cde5d6c216ae1f67673a1516f1e9b896
Talles Corradini <tallescorradini@gmail.com>
2021-10-28 22:17:38 -0300

    fix: missing ratings

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -36,6 +36,13 @@ const PlaceDetails = ({ place }) => {
         </Typography>
 
         <Box display="flex" justifyContent="space-between">
+          <Rating size="small" value={Number(place.rating)} readOnly />
+          <Typography gutterBottom variant="subtitle1">
+            out of {place.num_reviews} reviews
+          </Typography>
+        </Box>
+
+        <Box display="flex" justifyContent="space-between">
           <Typography variant="subtitle1">Price</Typography>
           <Typography gutterBottom variant="subtitle1">
             {place.price_level}
