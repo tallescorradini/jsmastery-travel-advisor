9351ac168ec7fe10c8ae30fa3efa62474799da2f
Talles Corradini <tallescorradini@gmail.com>
2021-11-03 22:43:34 -0300

    fix: add google api key and load it from environment variable

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Autocomplete, LoadScript } from "@react-google-maps/api";
+import { Autocomplete } from "@react-google-maps/api";
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 
@@ -30,25 +30,20 @@ const Header = ({ setCoordinates }) => {
             Explore new places
           </Typography>
 
-          <LoadScript
-            googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
-            libraries={places}
-          >
-            <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
-              <div className={classes.search}>
-                <div className={classes.searchIcon}>
-                  <SearchIcon />
-                </div>
-                <InputBase
-                  placeholder="Search…"
-                  classes={{
-                    root: classes.inputRoot,
-                    input: classes.inputInput,
-                  }}
-                />
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
               </div>
-            </Autocomplete>
-          </LoadScript>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+              />
+            </div>
+          </Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
